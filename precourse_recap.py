e7f5e53f14410335884fa7d3e8cd40803da19947
Ethan <ethanbaird94@gmail.com>
2022-06-20 15:53:45 +0100

    Created a file and added some entry level python

---
+++ precourse_recap.py
@@ -0,0 +1,18 @@
+str_1 = 'hello '
+str_2 = 'there'
+str_3 = '!'
+
+sentence = str_1.capitalize() + str_2 + str_3
+
+print (sentence)
+
+int_1 = 23
+int_2 = 47
+int_3 = 900
+
+sum = int_1 + int_2 + int_3
+avg = sum/3
+
+print (sum)
+
+print (avg)
